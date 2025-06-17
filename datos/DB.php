30b44a71cef3dad1d03ec5e45de0a467aec0a55a
jhaircv <jhaircasas34@gmail.com>
2025-06-17 16:37:31 -0500

    chamba para jose

--- datos/DB.php
+++ datos/DB.php
@@ -1,12 +1,12 @@
 <?php
-    $cadena = "mysql:host=localhost;port=3306;charset=utf8mb4";
+    $cadena = "mysql:host=localhost;dbname=blog;charset=utf8mb4";
     $user = "root";
     $password = "admin123";
-    $database = "blog";
+
     try {
         $conexion = new PDO($cadena, $user, $password);
+        
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conexion->exec("USE $database");
     } catch (PDOException $e) {
         echo "Error de conexión: " . $e->getMessage();
     }
